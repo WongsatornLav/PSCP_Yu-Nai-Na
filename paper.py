4dc7f2f6a4719904970dc197c30017c91cdc8dcf
ninenine78987 <nnarawit775@gmail.com>
2025-10-23 02:46:42 +0700

    Add PSCP_Yu-Nai-Na as a submodule

---
+++ paper.py
@@ -0,0 +1,13 @@
+'''
+doc
+'''
+def main():
+    '''
+    paper
+    '''
+    x = int(input().strip("A"))
+    y = int(input().strip("A"))
+    diff = abs(x - y)
+    res = pow(2, diff)
+    print(res)
+main()
